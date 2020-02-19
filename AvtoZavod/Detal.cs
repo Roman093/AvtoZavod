7e23cdb176efd367d3a60c0ba0422fa407e6843f
Roman093 <58482842+Roman093@users.noreply.github.com>
2020-02-20 00:07:04 +0300

    Add files via upload

---
+++ AvtoZavod/Detal.cs
@@ -0,0 +1,9 @@
+﻿namespace AvtoZavod
+{
+    public class Detal
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int CarId { get; set; }
+    }
+}
